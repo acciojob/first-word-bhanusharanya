172644918ff0256ef37538affec687edf698a2ac
bhanusharanya <sharanya2630@gmail.com>
2024-06-19 15:41:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function firstWord(s) {
-  // your code here
+  // Check if the string is empty
+  if (s.length === 0) {
+    return '';
+  }
+
+  // Find the index of the first space
+  const firstSpaceIndex = s.indexOf(' ');
+
+  // If there is no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring from the start to the first space
+  return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
